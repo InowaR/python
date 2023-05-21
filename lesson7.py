c1bc3568e7db116ead8ac5c1c00dbcc71a7ce3ce
Georgiy <georgii_sidenko@mail.ru>
2023-05-21 18:40:53 +0300

    2 task

--- lesson7.py
+++ lesson7.py
@@ -1,15 +1,31 @@
 # Задача 1. Создайте пользовательский аналог метода map().
 
-array = [1, 2, 3, 4, 5]
+# array = [1, 2, 3, 4, 5]
 
-def func(element):
-    return element * 2
+# def func(element):
+#     return element * 2
 
-def user_map(func, array):
-    new_array = []
-    for element in array:
-        new_array.append(func(element))
-    return new_array
+# def user_map(func, array):
+#     new_array = []
+#     for element in array:
+#         new_array.append(func(element))
+#     return new_array
 
-print(user_map(func, array))
-    
+# print(user_map(func, array))
+
+
+# Задача 2. Создайте декоратор, повторяющий функцию заданное количество раз.
+
+def repeat_func(times):
+    def procedure(func):
+        def wrapper():
+            for _ in range(times):
+                func()
+        return wrapper
+    return procedure
+
+@repeat_func(times=6)
+def func():
+    print("Hello")
+
+func()

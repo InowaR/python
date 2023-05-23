2e567f1240a8ca7dd54c79b7906a712e15022a9f
Georgiy <georgii_sidenko@mail.ru>
2023-05-24 01:01:04 +0300

    first task

--- lesson8.py
+++ lesson8.py
@@ -1,19 +1,33 @@
 # Задача 1. Напишите бота для техподдержки. Бот должен записывать обращения пользователей в файл.
 
 import telebot
-
+import ast
 bot = telebot.TeleBot('key')
 
 @bot.message_handler()
 def start(message):
-    # print(message)
-    # print(message.text)
-    # print(message.from_user.first_name)
-    # print(message.from_user.last_name)
-    messages = {}
-    messages[message.id] = message.from_user.first_name, message.from_user.last_name, message.text
-    f = open('dict.txt','a')
-    f.write(str(messages))
-    f.close()
+    messages = {
+                "first_name" : message.from_user.first_name, 
+                "last_name" : message.from_user.last_name,
+                "text" : message.text
+              }
+    
+    with open("messages.txt", "a") as f:
+        f.write(str(messages) + "\n")
 
 bot.polling()
+
+
+with open('messages.txt', 'r') as f:
+    file = f.readlines()
+
+for i in range(len(file)):
+    str = file[i]
+    dictionary = ast.literal_eval(str)
+    first_name = dictionary["first_name"]
+    last_name = dictionary["last_name"]
+    text = dictionary["text"]
+    print(first_name, last_name, text)
+
+# Задача 2. Напишите программу, которая позволяет считывать из файла вопрос,
+# отвечать на него и отправлять ответ обратно пользователю.

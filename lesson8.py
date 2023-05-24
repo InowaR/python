7f93af8c04280ff766d1a020860ce00c541061a4
Georgiy <georgii_sidenko@mail.ru>
2023-05-24 16:39:38 +0300

    task 2

--- lesson8.py
+++ lesson8.py
@@ -1,33 +1,45 @@
 # Задача 1. Напишите бота для техподдержки. Бот должен записывать обращения пользователей в файл.
 
-# import telebot
+import telebot
+import ast
+import json
+
+bot = telebot.TeleBot('key')
+
+@bot.message_handler()
+def start(message):
+    messages = {
+                'first_name' : message.from_user.first_name, 
+                'last_name' : message.from_user.last_name,
+                'text' : message.text
+              }
+    with open('messages.txt', 'a') as f:
+        f.write(str(messages) + '\n')
+
+bot.polling()
+
+
+# Создает новый файл answers.txt с ответами на сообщения пользователей
+
 # import ast
-# bot = telebot.TeleBot('key')
+# import json
 
-# @bot.message_handler()
-# def start(message):
-#     messages = {
-#                 'first_name' : message.from_user.first_name, 
-#                 'last_name' : message.from_user.last_name,
-#                 'text' : message.text
-#               }
-    
-#     with open('messages.txt', 'a') as f:
-#         f.write(str(messages) + '\n')
+# def make_reply_to_messages():
+#     with open('messages.txt', 'r') as f:
+#         messages = f.readlines()
+#     for i in range(len(messages)):
+#         str = messages[i]
+#         dictionary = ast.literal_eval(str)
+#         print(dictionary)
+#         print('Сообщение:', dictionary['text'])
+#         answer = input('Введите ответ на сообщение: ')
+#         dictionary['text'] = answer
+#         serialized = json.dumps(dictionary, ensure_ascii=False)
+#         print(serialized)
+#         with open('answers.txt', 'a') as f:
+#             f.write(serialized + '\n') 
 
-# bot.polling()
-
-
-# with open('messages.txt', 'r') as f:
-#     file = f.readlines()
-
-# for i in range(len(file)):
-#     str = file[i]
-#     dictionary = ast.literal_eval(str)
-#     first_name = dictionary['first_name']
-#     last_name = dictionary['last_name']
-#     text = dictionary['text']
-#     print(first_name, last_name, text)
+# make_reply_to_messages()  
 
 # Задача 2. Напишите программу, которая позволяет считывать из файла вопрос,
 # отвечать на него и отправлять ответ обратно пользователю.
@@ -65,25 +77,3 @@
 #     bot.reply_to(message, f'Сообщение пользователя: {question}\nОтвет на сообщение: {answer}')
 
 # bot.polling()
-
-
-# Создает новый файл answers.txt с ответами на сообщения пользователей
-import ast
-import json
-
-def make_reply_to_messages():
-    with open('messages.txt', 'r') as f:
-        messages = f.readlines()
-    for i in range(len(messages)):
-        str = messages[i]
-        dictionary = ast.literal_eval(str)
-        print(dictionary)
-        print('Сообщение:', dictionary['text'])
-        answer = input('Введите ответ на сообщение: ')
-        dictionary['text'] = answer
-        serialized = json.dumps(dictionary, ensure_ascii=False)
-        print(serialized)
-        with open('answers.txt', 'a') as f:
-            f.write(serialized + '\n') 
-
-make_reply_to_messages()    

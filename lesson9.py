ef8121f0df24ecfc563a701e4c2797e8022315a2
Georgiy <georgii_sidenko@mail.ru>
2023-05-30 00:03:48 +0300

    3 tasks

--- lesson9.py
+++ lesson9.py
@@ -11,7 +11,7 @@ for i, j in zip(unique, counts):
 
 # Задача 2. Создайте двумерный массив, размером 5х5. Определите, есть ли в нём одинаковые строки.
 
-two_dimensional_array = np.array([[1,1,1],[1,2,1],[1,2,1],[1,1,1]])
+two_dimensional_array = np.random.randint(2, size=(5, 5))
 unique_rows = np.unique(two_dimensional_array, axis=0)
 print(two_dimensional_array)
 if unique_rows.shape[0] < two_dimensional_array.shape[0]:
